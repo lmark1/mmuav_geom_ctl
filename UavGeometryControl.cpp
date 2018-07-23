9c5a684a630993972e6b73f1830a57871dba0670
Lovro Markovic <lovro.markovic@fer.hr>
2018-07-23 18:09:49 +0200

    Created main files

---
+++ UavGeometryControl.cpp
@@ -0,0 +1,18 @@
+/*
+ * UavGeometryControl.cpp
+ *
+ *  Created on: Jul 23, 2018
+ *      Author: lmark
+ */
+
+#include "UavGeometryControl.h"
+
+UavGeometryControl::UavGeometryControl() {
+	// TODO Auto-generated constructor stub
+
+}
+
+UavGeometryControl::~UavGeometryControl() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ UavGeometryControl.h
@@ -0,0 +1,17 @@
+/*
+ * UavGeometryControl.h
+ *
+ *  Created on: Jul 23, 2018
+ *      Author: lmark
+ */
+
+#ifndef SRC_MORUS_MODULES_MMUAV_GEOM_CTL_UAVGEOMETRYCONTROL_H_
+#define SRC_MORUS_MODULES_MMUAV_GEOM_CTL_UAVGEOMETRYCONTROL_H_
+
+class UavGeometryControl {
+public:
+	UavGeometryControl();
+	virtual ~UavGeometryControl();
+};
+
+#endif /* SRC_MORUS_MODULES_MMUAV_GEOM_CTL_UAVGEOMETRYCONTROL_H_ */

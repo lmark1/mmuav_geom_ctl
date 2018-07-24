1c3afe39a35430edc12e271cc4c18492fe25c90b
Lovro Markovic <lovro.markovic@fer.hr>
2018-07-24 15:27:22 +0200

    Added publishers and subscribers

--- UavGeometryControl.cpp
+++ UavGeometryControl.cpp
@@ -7,12 +7,157 @@
 
 #include "UavGeometryControl.h"
 
-UavGeometryControl::UavGeometryControl() {
-	// TODO Auto-generated constructor stub
+UavGeometryControl::UavGeometryControl() :
+	geometry_control_node_handle_(geometry_control_app_state_),
+	params_(),
+	geometry_control_loop_rate_(params_.controller_sample_rate),
 
+	geometry_control_task_(-1),
+	geometry_control_task_should_exit_(false),
+
+	armed_flag_(attitude_controller_reference_s::DISARMED),
+	roll_control_flag_previous_(attitude_controller_reference_s::OFF),
+	roll_control_flag_(attitude_controller_reference_s::OFF),
+	pitch_control_flag_previous_(attitude_controller_reference_s::OFF),
+	pitch_control_flag_(attitude_controller_reference_s::OFF),
+	yaw_control_flag_previous_(attitude_controller_reference_s::OFF),
+	yaw_control_flag_(attitude_controller_reference_s::OFF),
+	yaw_rate_control_flag_previous_(attitude_controller_reference_s::OFF),
+	yaw_rate_control_flag_(attitude_controller_reference_s::OFF)
+{
 }
 
-UavGeometryControl::~UavGeometryControl() {
-	// TODO Auto-generated destructor stub
+UavGeometryControl::~UavGeometryControl()
+{
+	if (geometry_control_task_ != -1)
+	{
+		geometry_control_task_should_exit_ = true;
+		int i = 0;
+		// Sleep for 0.5s to ensure task exits
+		while(geometry_control_task_ != -1)
+		{
+			usleep(10000);
+			if(++i > 50)
+			{
+				px4_task_delete(geometry_control_task_);
+				break;
+			}
+		}
+	}
+
+	uav_geometry_control::instance = nullptr;
 }
+
+int UavGeometryControl::start()
+{
+	ASSERT(geometry_control_task_ == -1);
+
+	// Start attitude control task
+	geometry_control_task_ = px4_task_spawn_cmd(
+			"mmc_vpc_attitude_control",
+			SCHED_DEFAULT,
+			SCHED_PRIORITY_MAX - 5,
+			1500,
+			(px4_main_t)&UavGeometryControl::geometryControlTaskTrampoline,
+			nullptr);
+
+	if (geometry_control_task_ < 0)
+	{
+		warn("Failed to start attitude control task! \n");
+		return -errno;
+	}
+
+	return OK;
+}
+
+void UavGeometryControl::geometryControlTaskTrampoline(int argc, char *argv[])
+{
+	uav_geometry_control::instance->geometryControlTaskMain();
+	PX4_INFO("Returning from geometry control task trampoline! \n");
+	return;
+}
+
+void UavGeometryControl::geometryControlTaskMain()
+{
+
+	// Node frequency is determined by param. 100Hz by default.
+	//attitude_control_loop_rate_ = px4::Rate(params_.attitude_sample_rate);
+
+	// Subscribers
+	geometry_control_node_handle_.subscribe<px4::px4_parameter_update>(
+			&UavGeometryControl::parametersUpdateCallback, this, 1);
+	geometry_control_node_handle_.subscribe<px4::px4_vehicle_attitude>(
+			&UavGeometryControl::vehicleAttitudeCallback, this, 1);
+	geometry_control_node_handle_.subscribe<px4::px4_attitude_controller_reference>(
+				&UavGeometryControl::attitudeReferenceCallback, this, 1);
+
+	// Publishers
+	moving_mass_setpoint_array_pub_ = geometry_control_node_handle_
+			.advertise<px4::px4_moving_mass_setpoint_array>();
+	gas_motor_setpoint_array_pub_ = geometry_control_node_handle_
+			.advertise<px4::px4_gas_motor_setpoint_array>();
+	attitude_controller_status_pub_ = geometry_control_node_handle_
+			.advertise<px4::px4_attitude_controller_status>();
+
+	float sample_time = 1.0f/((float)params_.controller_sample_rate);
+
+	while(!geometry_control_task_should_exit_)
+	{
+		// Sleep and collect callback data
+		geometry_control_loop_rate_.sleep();
+		geometry_control_node_handle_.spinOnce();
+	}
+}
+
+void UavGeometryControl::attitudeReferenceCallback(
+		const px4::px4_attitude_controller_reference &msg)
+{
+	attitude_reference_(0)    = msg.data().roll;
+	attitude_reference_(1)    = msg.data().pitch;
+	attitude_reference_(2)    = msg.data().yaw;
+	yaw_rate_feedforward_     = msg.data().yaw_rate;
+	rotor_speed_reference_    = msg.data().throttle;
+
+	// Flags
+	armed_flag_             = msg.data().armed;
+	roll_control_flag_      = msg.data().roll_control;
+	pitch_control_flag_     = msg.data().pitch_control;
+	yaw_control_flag_       = msg.data().yaw_control;
+	yaw_rate_control_flag_  = msg.data().yaw_rate_control;
+}
+
+void UavGeometryControl::vehicleAttitudeCallback(
+		const px4::px4_vehicle_attitude &msg)
+{
+	// Set message quaternion to matrix quaternion
+	matrix::Quatf q(
+			msg.data().q[0],
+			msg.data().q[1],
+			msg.data().q[2],
+			msg.data().q[3]);
+	// Quaternion to euler
+	matrix::Eulerf e(q);
+	//ut_test(isEqual(e1, euler_check));
+
+	// Attitude measured(NED), subtract roll and pitch offset
+	attitude_measured_(0) = e(0);
+	attitude_measured_(1) = e(1);
+	attitude_measured_(2) = e(2);
+
+	//PX4_INFO("%.2f, %.2f, %.2f", (double)attitude_measured_(0),
+	//  (double)attitude_measured_(1), (double)attitude_measured_(2));
+
+	// Rates measured(NED)
+	rates_measured_(0) = msg.data().rollspeed;
+	rates_measured_(1) = msg.data().pitchspeed;
+	rates_measured_(2) = msg.data().yawspeed;
+}
+
+void UavGeometryControl::parametersUpdateCallback(
+		const px4::px4_parameter_update &msg)
+{
+  PX4_INFO("Parameters updated!");
+  params_.update();
+}
+
 
--- UavGeometryControl.h
+++ UavGeometryControl.h
@@ -11,6 +11,15 @@
 // Pixhawk
 #include <px4.h>
 
+// Topics
+#include <platforms/nuttx/px4_messages/px4_attitude_controller_reference.h>
+#include <platforms/nuttx/px4_messages/px4_moving_mass_setpoint.h>
+#include <platforms/nuttx/px4_messages/px4_moving_mass_setpoint_array.h>
+#include <platforms/nuttx/px4_messages/px4_gas_motor_setpoint.h>
+#include <platforms/nuttx/px4_messages/px4_gas_motor_setpoint_array.h>
+#include <platforms/nuttx/px4_messages/px4_pid_status.h>
+#include <platforms/nuttx/px4_messages/px4_attitude_controller_status.h>
+
 // Standard
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,6 +28,17 @@
 #include <errno.h>
 #include <math.h>
 
+// Drivers
+#include <drivers/drv_hrt.h>
+
+// Math library
+#include <lib/mathlib/mathlib.h>
+#include <matrix/math.hpp>
+#include <matrix/filter.hpp>
+#include <matrix/integration.hpp>
+
+// Parameters
+#include "UavGeometryControlParams.h"
 
 class UavGeometryControl;
 
@@ -40,8 +60,53 @@ public:
 protected:
 
 	// Node handles for subscribers and publishers
-	px4::NodeHandle attitude_control_node_handle_;
-	px4::AppState attitude_control_app_state_;
+	px4::NodeHandle geometry_control_node_handle_;
+	px4::AppState geometry_control_app_state_;
+
+private:
+
+	// Geometry control task
+	static void geometryControlTaskTrampoline(int argc, char *argv[]);
+	void geometryControlTaskMain();
+
+	// Publishers
+	px4::Publisher<px4::px4_moving_mass_setpoint_array>
+		*moving_mass_setpoint_array_pub_;
+	px4::Publisher<px4::px4_gas_motor_setpoint_array>
+		*gas_motor_setpoint_array_pub_;
+	px4::Publisher<px4::px4_attitude_controller_status>
+		*attitude_controller_status_pub_;
+
+	// Topics callbacks
+	void vehicleAttitudeCallback(
+			const px4::px4_vehicle_attitude &msg);
+	void parametersUpdateCallback(
+			const px4::px4_parameter_update &msg);
+	void attitudeReferenceCallback(
+			const px4::px4_attitude_controller_reference &msg);
+
+	// Angle setpoints and measured value
+	float rotor_speed_reference_, yaw_rate_feedforward_;
+	math::Vector<3> attitude_reference_;
+	math::Vector<3> attitude_measured_;
+	math::Vector<3> rates_measured_;
+
+	// Controller flags
+	bool armed_flag_;
+	bool roll_control_flag_previous_, roll_control_flag_;
+	bool pitch_control_flag_previous_, pitch_control_flag_;
+	bool yaw_control_flag_previous_, yaw_control_flag_;
+	bool yaw_rate_control_flag_previous_, yaw_rate_control_flag_;
+
+	// Parameters object
+	UavGeometryControlParams params_;
+
+	// Controller loop rate
+	px4::Rate geometry_control_loop_rate_;
+
+	// Geometry control task ID and running check
+	int geometry_control_task_;
+	bool geometry_control_task_should_exit_;
 
 };
 

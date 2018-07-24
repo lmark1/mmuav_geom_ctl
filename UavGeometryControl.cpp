4f98614531cd8a385021b3ffc524bff57dbcc27c
Lovro Markovic <lovro.markovic@fer.hr>
2018-07-24 15:56:39 +0200

    Started attitude tracking implementation

--- UavGeometryControl.cpp
+++ UavGeometryControl.cpp
@@ -7,6 +7,45 @@
 
 #include "UavGeometryControl.h"
 
+const double G = 9.81;
+
+// UAV constants
+const double UAV_MASS = 2.1;
+const double ARM_LENGTH = 0.314;
+const double MOMENT_CONSTANT = 0.016;
+const double MOTOR_CONSTANT = 8.54858e-06;
+
+// ROTOR constants
+const double ROTOR_MASS = 0.01;
+const double ROTOR_VELOCITY_SLOWDOWN_SIM = 15;
+const double ROTOR_RADIUS = 0.1524;
+const double MIN_ROTOR_VELOCITY = 0;
+const double MAX_ROTOR_VELOCITY = 1475;
+Matrix<double, 3, 3> INERTIA;
+const double D =  ARM_LENGTH + ROTOR_RADIUS / 2;
+const double MAXIMUM_MOMENT =
+		MAX_ROTOR_VELOCITY * MAX_ROTOR_VELOCITY * MOTOR_CONSTANT // MAX FORCE
+		* D;
+
+// Moving mass constants
+const double MM_MASS = 0.208;
+const double MM_FORCE = MM_MASS * G;
+
+// Transform matrix with roll / pitch corrections
+Matrix<double, 4, 4> THRUST_TRANSFORM_FULL;
+
+// Transform matrix without roll / pitch corrections
+Matrix<double, 4, 4> THRUST_TRANSFORM_YAW;
+
+const Matrix<double, 3, 1> E3;
+Matrix<double, 3, 3> EYE3;
+Matrix<double, 4, 4> EYE4;
+
+// Define control modes
+const int POSITION_CONTROL = 1;
+const int ATTITUDE_CONTROL = 2;
+const int VELOCITY_CONTROL = 3;
+
 UavGeometryControl::UavGeometryControl() :
 	geometry_control_node_handle_(geometry_control_app_state_),
 	params_(),
@@ -23,7 +62,9 @@ UavGeometryControl::UavGeometryControl() :
 	yaw_control_flag_previous_(attitude_controller_reference_s::OFF),
 	yaw_control_flag_(attitude_controller_reference_s::OFF),
 	yaw_rate_control_flag_previous_(attitude_controller_reference_s::OFF),
-	yaw_rate_control_flag_(attitude_controller_reference_s::OFF)
+	yaw_rate_control_flag_(attitude_controller_reference_s::OFF),
+
+	current_control_mode_(ATTITUDE_CONTROL)
 {
 }
 
@@ -160,4 +201,148 @@ void UavGeometryControl::parametersUpdateCallback(
   params_.update();
 }
 
+void UavGeometryControl::hatOperator(
+		const double x,
+		const double y,
+		const double z,
+		Matrix<double, 3, 3> &hatMatrix)
+{
+	hatMatrix.setZero();
+	hatMatrix(0, 1) = -z;
+	hatMatrix(0, 2) =  y;
+	hatMatrix(1, 0) =  z;
+	hatMatrix(1, 2) = -x;
+	hatMatrix(2, 0) = -y;
+	hatMatrix(2, 1) =  x;
+}
+
+void UavGeometryControl::veeOperator(
+		Matrix<double, 3, 3> hatMatrix,
+		Matrix<double, 3, 1> &veeVector)
+{
+	veeVector.setZero();
+	veeVector(0, 0) = hatMatrix(2, 1); 			// x component
+	veeVector(1, 0) = hatMatrix(0, 2);			// y component
+	veeVector(2, 0) = hatMatrix(1, 0);			// z component
+}
+
+void UavGeometryControl::euler2RotationMatrix(
+		const double roll,
+		const double pitch,
+		const double yaw,
+		Matrix<double, 3, 3> &rotMatrix)
+{
+	rotMatrix.setZero();
+	rotMatrix(0, 0) = cos(yaw) * cos(pitch);
+	rotMatrix(0, 1) = cos(yaw) * sin(pitch) * sin(roll) - sin(yaw) * cos(roll);
+	rotMatrix(0, 2) = cos(yaw) * sin(pitch) * cos(roll) + sin(yaw) * sin(roll);
+	rotMatrix(1, 0) = sin(yaw) * cos(pitch);
+	rotMatrix(1, 1) = sin(yaw) * sin(pitch) * sin(roll) + cos(yaw) * cos(roll);
+	rotMatrix(1, 2) = sin(yaw) * sin(pitch) * cos(roll) - cos(yaw) * sin(roll);
+	rotMatrix(2, 0) = - sin(pitch);
+	rotMatrix(2, 1) = cos(pitch) * sin(roll);
+	rotMatrix(2, 2) = cos(pitch) * cos(roll);
+}
+
+void UavGeometryControl::attitudeTracking(
+		const Matrix<double, 3, 1> b1_desired,
+		const Matrix<double, 3, 1> b3_desired,
+		const double dt,
+		Matrix<double, 3, 3> &R_c_old,
+		Matrix<double, 3, 3> &omega_c_old,
+		Matrix<double, 3, 1> &M_u)
+{
+
+	// Attitude errors
+	Matrix<double, 3, 1> e_omega, e_R;
+
+	// Auxiliary skew matrices
+	Matrix<double, 3, 3> e_R_skew, omega_mv_skew, omega_c_skew,
+						 alpha_c_skew;
+
+	if (current_control_mode_ == POSITION_CONTROL)
+	{
+		//cout << "Attitude: POSITION" << "\n";
+		/**
+		 * During position control desired rotation, angular velocity
+		 * and angular acceleration matrices will be CALCULATED.
+		 * R_c, omega_c, alpha_c
+		 */
+		Matrix<double, 3, 3> R_c;
+		Matrix<double, 3, 1> b1_c, b2_c;
+
+		/*
+		 * b13_normal - Normal of plane spanned by b3_d and b1_d.
+		 *
+		 * Note: b1_d will not necessarily lie in the plane with b3_d normal,
+		 * it is needed to calculate it's projection to that plane.
+		 */
+		Matrix<double, 3, 1> b13_normal = b3_desired.cross(b1_desired);
+
+		// Compute b1_c = Proj[b1_d] onto the plane with normal b3_d
+		b1_c = - b3_desired.cross(b13_normal) / b13_normal.norm();
+
+		// Construct desired rotation matrix
+		b2_c = b3_desired.cross(b1_c);
+		b2_c = b2_c / b2_c.norm();
+		R_c.setZero(3, 3);
+		R_c << b1_c, b2_c, b3_desired;
+
+		calculateDesiredAngularVelAndAcc(R_c, R_c_old, R_mv_, omega_c_old, dt);
+
+		// Remap calculated to desired
+		R_d_ = R_c;
+
+
+		// Update old R_c
+		R_c_old = R_c;
+	}
+	else if (current_control_mode_ == ATTITUDE_CONTROL)
+	{
+		//cout << "Attitude: ATTITUDE" << "\n";
+		// Do nothing here - read desired attitude values from
+		// callback functions.
+		euler2RotationMatrix(
+				(double)euler_d_(0,0),
+				(double)euler_d_(1,0),
+				(double)euler_d_(2,0),
+				R_d_);
+	}
+	else
+	{
+		ROS_ERROR("Invalid control mode given.");
+		throw std::runtime_error("Invalid control mode given.");
+	}
+
+	cout << "R_d:\n" << R_d_ << "\n";
+	cout << "R_mv:\n" << R_mv_ << "\n";
+
+	// ATTITUDE TRACKING
+	// Calculate control moment M
+	e_R_skew = (R_d_.adjoint() * R_mv_ - R_mv_.adjoint() * R_d_) / 2;
+	veeOperator(e_R_skew, e_R);
+	e_omega = (omega_mv_ - R_mv_.adjoint() * R_d_ * omega_d_);
+	if (e_omega(0, 0) != e_omega(0, 0))
+	{
+		throw std::runtime_error("STOP");
+	}
+	hatOperator(
+			(double)omega_mv_(0, 0),
+			(double)omega_mv_(1, 0),
+			(double)omega_mv_(2, 0),
+			omega_mv_skew);
+
+	M_u = 	- k_R_ * e_R
+			- k_omega_ * e_omega
+			+ omega_mv_.cross(INERTIA * omega_mv_)
+			- INERTIA *
+			(
+				omega_mv_skew * R_mv_.adjoint() * R_d_ * omega_d_
+				- R_mv_.adjoint() * R_d_ * alpha_d_
+			);
+
+	M_u(0, 0) = saturation((double)M_u(0, 0), -MAXIMUM_MOMENT, MAXIMUM_MOMENT);
+	M_u(1, 0) = saturation((double)M_u(1, 0), -MAXIMUM_MOMENT, MAXIMUM_MOMENT);
+	M_u(2, 0) = saturation((double)M_u(2, 0), -MAXIMUM_MOMENT, MAXIMUM_MOMENT);
+}
 
--- UavGeometryControl.h
+++ UavGeometryControl.h
@@ -42,6 +42,8 @@
 
 class UavGeometryControl;
 
+using namespace matrix;
+
 namespace uav_geometry_control
 {
   extern UavGeometryControl *instance;
@@ -68,6 +70,116 @@ private:
 	// Geometry control task
 	static void geometryControlTaskTrampoline(int argc, char *argv[]);
 	void geometryControlTaskMain();
+
+	/**
+	 * Calculate b3_d and f_u as position tracking control inputs.
+	 *
+	 * @param pos_desired - desired position reference
+	 * 						(may change due to prefilter)
+	 * @param pos_old - old position
+	 * @param dt - time interval
+	 * @param b3_d - thrust heading reference, assigned in method
+	 * @param f_u - thrust magnitude value, assigned in method
+	 */
+	void trajectoryTracking(
+			const Matrix<double, 3, 1> pos_desired,
+			const Matrix<double, 3, 1> pos_old,
+			const double dt,
+			Matrix<double, 3, 1> &b3_d,
+			double &f_u);
+
+	/**
+	 * Calculate control moments M_u used for attitude tracking.
+	 *
+	 * @param b1_d - desired heading
+	 * @param b3_d - desired thrust vector
+	 * @param dt - time interval
+	 * @param R_c_old - reference for old calculated rotation matrix
+	 * 					(Position tracking)
+	 * @param omega_c_old - reference for old calculated angular velocity
+	 * 						(Position tracking)
+	 * @param M_u - control moments, assigned in method
+	 */
+	void attitudeTracking(
+			const Matrix<double, 3, 1> b1_desired,
+			const Matrix<double, 3, 1> b3_desired,
+			const double dt,
+			Matrix<double, 3, 3> &R_c_old,
+			Matrix<double, 3, 3> &omega_c_old,
+			Matrix<double, 3, 1> &M_u);
+
+	/**
+	 * Calculate and set desired angular velocity and
+	 * desired angular acceleration based on R_c[k] and R_c[k-1].
+	 *
+	 * @param R_c - Calculated R
+	 * @param R_c_old - Calculated old R
+	 * @param R_mv - Measured R
+	 * @param omega_c_old - Reference to old angular velocity value
+	 * @param dt
+	 */
+	void calculateDesiredAngularVelAndAcc(
+			const Matrix<double, 3, 3> R_c,
+			const Matrix<double, 3, 3> R_c_old,
+			const Matrix<double, 3, 3> R_mv,
+			Matrix<double, 3, 3> &omega_c_old,
+			double dt);
+
+	/**
+	 * Calculate rotor velocities from given vector containing
+	 * total thrust and moments M_x, M_y, M_z.
+	 *
+	 * @param thrust_moment_vec
+	 * @param transform_matrix - matrix transforms given vector to forces
+	 * @rotor_velocities
+	 */
+	void calculateRotorVelocities(
+			Matrix<double, 4, 1> thrust_moment_vec,
+			Matrix<double, 4, 4> transform_matrix,
+			Matrix<double, 4, 1>& rotor_velocities);
+
+
+	/**
+	 * Perform hat operator on given vector components.
+	 *
+	 * @param x - x vector component
+	 * @param y - y vector component
+	 * @param z - z vector component
+	 * @param hatMatrixs - Matrix of the following form:
+	 * 	[ 0  -z,  y]
+	 * 	[ z,  0, -x]
+	 * 	[-y,  x,  0]
+	 */
+	void hatOperator(
+			const double x,
+			const double y,
+			const double z,
+			Matrix<double, 3, 3> &hatMatrix);
+
+	/**
+	 * Perform a vee( V ) operator on a given hatMatrix.
+	 * It is implied that the given hatMatrix is a skew matrix.
+	 * It will decompose the skew matrix into a given veeVector reference.
+	 *
+	 * @param hatMatrx
+	 * @param veeVector
+	 */
+	void veeOperator(
+			const Matrix<double, 3, 3> hatMatrix,
+			Matrix<double, 3, 1> &veeVector);
+	/**
+	 * Euler angles represented as a rotation matrix.
+	 *
+	 * @param roll
+	 * @param pitch
+	 * @param yaw
+	 * @param rotMatrix - Rotation matrix will be stored here
+	 */
+	void euler2RotationMatrix(
+			const double roll,
+			const double pitch,
+			const double yaw,
+			Matrix<double, 3, 3> &rotMatrix);
 
 	// Publishers
 	px4::Publisher<px4::px4_moving_mass_setpoint_array>
@@ -107,6 +219,14 @@ private:
 	// Geometry control task ID and running check
 	int geometry_control_task_;
 	bool geometry_control_task_should_exit_;
+
+
+	/* Current control mode:
+	 * 	- position control
+	 * 	- attitude control
+	 * 	- linear velocity control
+	 */
+	int current_control_mode_;
 
 };
 
